569073ba46dfa6f1b1128bcfa1c7fffabf868638
jhva <{junghun8158@naver.com}>
2022-09-26 22:32:19 +0900

    선형검색 알고리즘

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("안녕하세요");
     }
 }
---
+++ src/main/java/org/example/practice/maxArrayFor.java
@@ -0,0 +1,16 @@
+package org.example.practice;
+
+public class maxArrayFor {
+    public static void main(String[] args) {
+        int[] array = new int[]{11, 14, 13, 2};
+
+        int max = array[2];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                System.out.println(max);
+            }
+        }
+    }
+}
