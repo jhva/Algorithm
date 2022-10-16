40d3845431ebccc3ab403698bfeb9bebbe428a29
jhva <junghun8158@naver.com>
2022-10-16 20:35:37 +0700

    2839번 수정

--- src/main/java/org/example/기본수학1/설탕배달_2839.java
+++ src/main/java/org/example/기본수학1/설탕배달_2839.java
@@ -1,19 +1,26 @@
 package main.java.org.example.기본수학1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class 설탕배달_2839 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
         int bongZi = 0;
+
+
         while (true) {
             //18
             if (N % 5 == 0) {
                 //18/5 3
                 bongZi += N / 5;
+                System.out.println(bongZi);
                 break;
             } else {
                 //N에서 -3 을 해준다는 생각을못함 .
@@ -21,11 +28,9 @@ public class 설탕배달_2839 {
                 bongZi++;
             }
             if (N < 0) {
-                System.out.println(-1);
+                System.out.println("-1");
                 break;
             }
         }
-        System.out.println(bongZi);
-
     }
 }
