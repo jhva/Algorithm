c4fd575553799c6fc4c1f182f072a964bb9bbe3d
jhva <junghun8158@naver.com>
2022-10-20 13:40:16 +0700

    25305커트라인

---
+++ src/main/java/org/example/정렬/커트라인_25305.java
@@ -0,0 +1,30 @@
+package main.java.org.example.정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * 커트라인이란 상을 받는 사람들 중 점수가 가장 가장 낮은 사람의 점수를 말한다.
+ */
+public class 커트라인_25305 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();// 응시자 수
+        int k = sc.nextInt();//상을 받는 사람 몇등 까지 ?
+        int crt = 0;
+        Integer[] arr = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[N - k]);
+        System.out.println(arr[3]);
+
+    }
+}
