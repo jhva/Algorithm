3e09e7904273ceeb775bcb8e1b31fb092640d72d
jhva <junghun8158@naver.com>
2022-11-03 19:52:10 +0700

    하노이의 탑 이동순서

---
+++ src/main/java/org/example/재귀/하노이의탑이동순서_11729.java
@@ -0,0 +1,39 @@
+package org.example.재귀;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+public class 하노이의탑이동순서_11729 {
+
+    static StringBuffer sb;
+    static int count = 0;
+
+    private static void move(int from, int use, int to, int N) {
+        if (N == 1) {
+            count++;
+            sb.append(from + " " + to).append('\n');
+            return;
+        }
+
+        move(from, to, use, N - 1); // 마지막에  스타트 1 , 2, 3
+        sb.append(from + " " + to).append('\n');
+        count++;
+        move(use, from, to, N - 1); // 마지막 2,3,1
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuffer();
+
+
+        int N = Integer.parseInt(br.readLine());
+        move(1, 2, 3, N); // 원판의 개수 , 현재타워번호, 목표타워 번호 ,서브타워 번호;
+        System.out.println(count);
+        System.out.println(sb);
+    }
+
+
+}
