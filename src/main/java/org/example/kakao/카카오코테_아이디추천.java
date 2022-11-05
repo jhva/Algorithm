f340b76183dbdbf9d75766e7968671ad01ceab91
jhva <junghun8158@naver.com>
2022-11-05 16:05:43 +0700

    kakao 2021 블라인드 코테 신규아이디추천

---
+++ src/main/java/org/example/kakao/카카오코테_아이디추천.java
@@ -0,0 +1,49 @@
+package org.example.kakao;
+
+import java.util.*;
+
+public class 카카오코테_아이디추천 {
+    public static void main(String[] args) {
+        soulution("...!@BaT#*..y.abcdefghijklm");
+
+    }
+
+    public static boolean isValid(char c) {
+        //문자나 숫자는 유효한
+        if (Character.isLetterOrDigit(c)) return true;
+        if (c == '-' || c == '_' || c == '.') return true;
+        return false;
+    }
+
+    public static String soulution(String user_id) {
+
+
+        StringBuilder answer = new StringBuilder();
+
+        boolean lastDot = false;
+
+        for (char ch : user_id.toCharArray()) {
+            if (isValid(ch) == false) continue;
+            if (ch == '.') {
+                if (answer.length() == 0 || lastDot) continue;
+                lastDot = true;
+            } else {
+                lastDot = false;
+            }
+            ch = Character.toLowerCase(ch);
+            answer.append(ch);
+        }
+        if (answer.length() >= 16) answer.setLength(15);
+        if (answer.length() == 0) answer.append('a');
+        if (answer.charAt(answer.length() - 1) == '.') answer.deleteCharAt(answer.length() - 1);
+        if (answer.length() <= 2) {
+            char ch = answer.charAt(answer.length() - 1);
+            while (answer.length() < 3) {
+                answer.append(ch);
+            }
+        }
+        System.out.println(answer.toString());
+        return answer.toString();
+    }
+
+}
