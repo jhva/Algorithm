7cfb1f1d6515e89fe74ad804f3fea3bf94847b2b
jhva <junghun8158@naver.com>
2022-11-06 21:30:46 +0700

    체스판 solution 4

--- src/main/java/org/example/브루트포스/체스판다시칠하기_1018.java
+++ src/main/java/org/example/브루트포스/체스판다시칠하기_1018.java
@@ -98,73 +98,131 @@ import java.util.*;
 //        min = Math.min(min, cnt);
 //    }
 //}
+//
+//public class 체스판다시칠하기_1018 {
+//    static int board(char[][] myBoard, int x, int y) {
+//        char[][] BBoard = {
+//                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+//                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+//                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+//                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+//                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+//                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+//                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+//                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+//        };
+//        char[][] WBoard = {
+//                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+//                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+//                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+//                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+//                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+//                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+//                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+//                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+//        };
+//        int countB = 0;
+//        int countW = 0;
+//        for (int i = 0; i < 8; i++) {
+//            for (int j = 0; j < 8; j++) {
+//                if (BBoard[i][j] != myBoard[x + i][y + j]) {
+//                    countB++;
+//                }
+//                if (WBoard[i][j] != myBoard[x + i][y + j]) {
+//                    countW++;
+//                }
+//            }
+//        }
+//        return Math.min(countW, countB);
+//
+//
+//    }
+//
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        int N = Integer.parseInt(st.nextToken());
+//        int M = Integer.parseInt(st.nextToken());
+//
+//        char[][] myboard = new char[N][M];
+//
+//        for (int i = 0; i < N; i++) {
+//            String s = br.readLine();
+//            for (int j = 0; j < M; j++) {
+//                myboard[i][j] = s.charAt(j);
+//            }
+//        }
+//        int min = board(myboard, 0, 0);
+//
+//        for (int i = 0; i < N - 8; i++) {
+//            for (int j = 0; j < N - 8; j++) {
+//                min = min < board(myboard, i, j) ? min : board(myboard, i, j);
+//            }
+//        }
+//        System.out.println(min);
+//    }
+//}
 
 public class 체스판다시칠하기_1018 {
-    static int board(char[][] myBoard, int x, int y) {
-        char[][] BBoard = {
-                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
-                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
-                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
-                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
-                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
-                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
-                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
-                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
-        };
-        char[][] WBoard = {
-                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
-                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
-                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
-                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
-                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
-                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
-                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
-                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
-        };
-        int countB = 0;
-        int countW = 0;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (BBoard[i][j] != myBoard[x + i][y + j]) {
-                    countB++;
-                }
-                if (WBoard[i][j] != myBoard[x + i][y + j]) {
-                    countW++;
-                }
-            }
-        }
-        return Math.min(countW, countB);
-
-
-    }
+    //만들어지는 경우의 수 블랙보드 , 화이트보드
+    static final char[][] BBoard = {
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+    };
+    static final char[][] WBoard = {
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+    };
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int row = Integer.parseInt(st.nextToken());
+        int column = Integer.parseInt(st.nextToken());
+        String[] board = new String[row]; //입력한 보드
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int min = Integer.MAX_VALUE;
 
-        char[][] myboard = new char[N][M];
-
-        for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < M; j++) {
-                myboard[i][j] = s.charAt(j);
-            }
+        for (int i = 0; i < row; i++) {
+            board[i] = br.readLine(); //board 입력
         }
-        int min = board(myboard, 0, 0);
 
-        for (int i = 0; i < N - 8; i++) {
-            for (int j = 0; j < N - 8; j++) {
-                min = min < board(myboard, i, j) ? min : board(myboard, i, j);
+        for (int i = 0; i <= row - 8; i++) {
+            for (int j = 0; j <= column - 8; j++) {
+                int change1 = 0;
+                int change2 = 0;
+
+                for (int k = j; k < j + 8; k++) {
+                    for (int l = i; l < i + 8; l++) {
+                        if (board[l].charAt(k) != WBoard[l - i][k - j]) change1++;
+                        if (board[l].charAt(k) != BBoard[l - i][k - j]) change2++;
+                    }
+                }
+                if (change1 < min || change2 < min) {
+                    min = Math.min(change1, change2);
+                }
             }
         }
         System.out.println(min);
     }
 }
+
 /**
  * 체스판은 검은색과 흰색이 번갈아서 칠해져 있어야한다 .
  * <p>
