6bba0710ea1f9ec88411452e90551aaa09e418a5
jhva <junghun8158@naver.com>
2022-11-21 20:50:37 +0700

    2675 문자열반복

---
+++ src/main/java/org/example/문자열반복_2675.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class 문자열반복_2675 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int a = sc.nextInt();
+            String s = sc.next();
+            for (int j = 0; j < s.length(); j++) {
+                for (int k = 0; k < a; k++) {
+                    System.out.print(s.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
