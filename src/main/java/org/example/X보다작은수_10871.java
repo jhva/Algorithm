7f7791c698ab94d6338439af21a229c5052243f4
jhva <junghun8158@naver.com>
2022-11-21 18:14:05 +0700

    x보다작은수

---
+++ src/main/java/org/example/X보다작은수_10871.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class X보다작은수_10871 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] += Integer.parseInt(st.nextToken());
+            if (arr[i] < X) {
+                System.out.print(arr[i] + " ");
+
+            }
+        }
+    }
+}
