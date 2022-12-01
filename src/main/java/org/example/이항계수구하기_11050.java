283d42485f2d05ad319a4be5fdbdbd885b17c9d4
jhva <junghun8158@naver.com>
2022-12-01 18:31:43 +0700

    이항계수구하기1 11050

---
+++ src/main/java/org/example/이항계수구하기_11050.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class 이항계수구하기_11050 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        System.out.println(calc(N) / (calc(N - K) * calc(K)));
+
+
+    }
+
+    public static int calc(int N) {
+        if (N <= 1) return 1;
+        return N * calc(N - 1);
+    }
+}
+
+//이항 계수(binomial coefficient)는 n개의 서로 다른 원소 중에서 r개의 원소를 순서없이 골라내는 방법의 수　
+/**
+ * N! / K! (N-K)!
+ * 자연수 \(N\)과 정수 \(K\)가 주어졌을 때 이항 계수
+ * \(\binom{N}{K}\)를 구하는 프로그램을 작성하시오.
+ * <p>
+ * <p>
+ * <p>
+ * 예제입력
+ * 5 2
+ * 5가지 경우의 수중 2개를 선택하는 경우의 수
+ * 5c2
+ * 5 * 4
+ * =======
+ * 2 * 1
+ * = 10
+ */
--- src/main/java/org/example/직각삼각형_4153.java
+++ src/main/java/org/example/직각삼각형_4153.java
@@ -1,30 +1,44 @@
 package org.example;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class 직각삼각형_4153 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            int z = sc.nextInt();
+        int arr[] = new int[3];
 
-            if (x == 0 && y == 0 && z == 0) {
+        while (true) {
+//            int x = sc.nextInt();
+//            int y = sc.nextInt();
+//            int z = sc.nextInt();
+//
+//            if (x == 0 && y == 0 && z == 0) {
+//                break;
+//            }
+//
+//            if ((x * x + y * y) == z * z) {
+//                System.out.println("right");
+//            } else if (x * x == (y * y + z * z)) {
+//                System.out.println("right");
+//            } else if (y * y == (z * z + x * x)) {
+//                System.out.println("right");
+//            } else {
+//                System.out.println("wrong");
+//            }
+            arr[0] = sc.nextInt();
+            arr[1] = sc.nextInt();
+            arr[2] = sc.nextInt();
+
+            if (arr[0] + arr[1] + arr[2] == 0) {
                 break;
             }
+            Arrays.sort(arr);
 
-            if ((x * x + y * y) == z * z) {
-                System.out.println("right");
-            } else if (x * x == (y * y + z * z)) {
-                System.out.println("right");
-            } else if (y * y == (z * z + x * x)) {
-                System.out.println("right");
-            } else {
-                System.out.println("wrong");
-            }
+            if (arr[2] * arr[2] == arr[0] * arr[0] + arr[1] * arr[1]) System.out.println("right");
+            else System.out.println("wrong");
 
         }
 
