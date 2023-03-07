0d8eb4e33e910147da1814bfdd25150e433d9525
jhva <junghun8158@naver.com>
2023-03-07 11:03:48 +0900

    큰 정수구하기 hackerank

--- src/main/java/org/example/hacker/medium/ExtraLongFactorials.java
+++ src/main/java/org/example/hacker/medium/ExtraLongFactorials.java
@@ -1,10 +1,23 @@
 package org.example.hacker.medium;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class ExtraLongFactorials {
     public static void main(String[] args) {
-//        BigDecimal s =BigDecimal.valueOf(
+        solve(30);
+    }
+
+    static void solve(int n) {
+        BigInteger bi = new BigInteger("1");
+        for (int i = 2; i <= n; i++) {
+            bi = bi.multiply(BigInteger.valueOf(i));
+            System.out.println(bi);
+            /**
+             * 큰 정 수 구하기
+             */
+        }
+        System.out.println(bi);
     }
 
 
