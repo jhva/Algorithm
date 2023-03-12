aa4f702fccb987e9f7780bea304f01bd58c8a15f
jhva <junghun8158@naver.com>
2023-03-12 22:35:19 +0900

    백준 신기한소수 completed , level3순위 failure

--- src/main/java/org/example/baekjoon/test.java
+++ src/main/java/org/example/baekjoon/test.java
@@ -1,17 +1,35 @@
 package org.example.baekjoon;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class test {
     public static void main(String[] args) {
-        testMethod(4);
+        testMethod(22, 9);
     }
 
-    public static void testMethod(int num) {
+    public static void testMethod(int num, int k) {
+        int index = 0;
 
-        if (num == 0) {
-            return;
+        try {
+            long temp = Long.valueOf(num);
+
+            while (temp % k != 0) {
+                String temp2 = String.valueOf(num);
+                temp += Long.parseLong(temp2);
+
+
+                index++;
+            }
+        } catch (NumberFormatException e) {
+            index = -1;
         }
-        System.out.println("안녕하세요");
-
-        testMethod(num - 1);
+        System.out.println(index + 1);
+//        if (num == 0) {
+//            return;
+//        }
+//        System.out.println("안녕하세요");
+//
+//        testMethod(num - 1);
     }
 }
