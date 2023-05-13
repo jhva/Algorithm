e68827243a00f0723b46dc99767b4af7f9c01277
jhva <junghun8158@naver.com>
2023-05-13 21:44:09 +0900

    level3 합승택시요금 수정

--- src/main/java/org/example/programmers/level3/level3합승택시요금.java
+++ src/main/java/org/example/programmers/level3/level3합승택시요금.java
@@ -19,7 +19,7 @@ public class level3합승택시요금 {
 
         @Override
         public int compareTo(Edge o) {
-            return this.cost - o.cost;
+            return cost;
         }
     }
 
@@ -30,7 +30,11 @@ public class level3합승택시요금 {
     public static void main(String[] args) {
         solve(6, 4, 6, 2,
             new int[][] {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66},
-                {2, 3, 22},{1,6,25}});
+                {2, 3, 22}, {1, 6, 25}});
+
+        플로이드_워셜(6, 4, 6, 2,
+            new int[][] {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66},
+                {2, 3, 22}, {1, 6, 25}});
     }
 
     static void solve(int n, int s, int a, int b, int[][] fares) {
@@ -44,7 +48,7 @@ public class level3합승택시요금 {
         }
 
         for (int[] i : fares) {
-            System.out.println(i[0]);
+            // System.out.println(i[0]);
             graph.get(i[0]).add(new Edge(i[1], i[2]));
             graph.get(i[1]).add(new Edge(i[0], i[2]));
         }
@@ -57,20 +61,20 @@ public class level3합승택시요금 {
         Arrays.fill(startB, MAX);
         Arrays.fill(start, MAX);
 
-        System.out.println(Arrays.toString(startA));
+        // System.out.println(Arrays.toString(startA));
 
         startA = dijkstra(a, startA);
         startB = dijkstra(b, startB);
         start = dijkstra(s, start);
-        System.out.println(Arrays.toString(startA));
-        System.out.println(Arrays.toString(start));
+        // System.out.println(Arrays.toString(startA));
+        // System.out.println(Arrays.toString(start));
 
-        for (int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             answer = Math.min(answer, startA[i] + startB[i] + start[i]);
 
         }
 
-        System.out.println(answer);
+        // System.out.println(answer);
 
     }
 
@@ -86,7 +90,8 @@ public class level3합승택시요금 {
             int nIndex = now.index;
             int nCost = now.cost;
 
-            if (nCost > costs[nIndex]) continue;
+            if (nCost > costs[nIndex])
+                continue;
 
             ArrayList<Edge> edges = graph.get(nIndex);
 
@@ -102,4 +107,28 @@ public class level3합승택시요금 {
         return costs;
 
     }
+    //플루이드워셜로풀기
+
+    static void 플로이드_워셜(int n, int s, int a, int b, int[][] fares) {
+        //결국은 모든정점에서의거리를구하고 최단거리를구해야함
+
+        //다익스트라가 성능적으로 시간, 공간 복잡도에서 많이유리하다
+
+        int[][] map = new int[n + 1][n + 1];
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                map[i][j] = 20000001;
+            }
+            map[i][i] = 0;
+        }
+        for (int i = 0; i < fares.length; i++) {
+            map[fares[i][0]][fares[i][1]] = fares[i][2];
+            map[fares[i][1]][fares[i][0]] = fares[i][2];
+        }
+
+        System.out.println(Arrays.deepToString(map));
+
+    }
+
 }
