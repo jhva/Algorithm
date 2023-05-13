0ec41104a19d5ff9172f47a680e0251a2e88dcb7
jhva <junghun8158@naver.com>
2023-05-13 21:41:45 +0900

    level2 파일명정렬

--- src/main/java/org/example/programmers/level2/level2프렌즈4블록.java
+++ src/main/java/org/example/programmers/level2/level2프렌즈4블록.java
@@ -2,6 +2,33 @@ package org.example.programmers.level2;
 
 public class level2프렌즈4블록 {
     public static void main(String[] args) {
+        String[] str = new String[] {"TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"};
+        solve(4, 5, str);
+    }
+
+    static void solve(int m, int n, String[] board) {
+        int answer = 0;
+        for (int i = 0; i < board.length; i++) {
+
+            for(int j=i+1; j< board.length; j++){
+                char s = (char)board[i].charAt(i);
+                char t =(char)board[j].charAt(j);
+
+
+
+                // System.out.println(s);
+                // System.out.println(t);
+
+            }
+
+
+
+        }
+
+    }
+    static void check(int start){
+
+        System.out.println(start);
 
     }
 }
--- src/main/java/org/example/programmers/level3/level3불량사용자.java
+++ src/main/java/org/example/programmers/level3/level3불량사용자.java
@@ -49,44 +49,52 @@ import java.util.HashSet;
 //     // }
 // }
 
-
 public class level3불량사용자 {
     private static HashSet<String> ban_user_idx;
-    public int solution(String[] user_id, String[] banned_id) {
+
+    public static void main(String[] args) {
+        solution(new String[] {"frodo", "fradi", "crodo", "abc123", "frodoc"},
+            new String[] {"*rodo", "*rodo", "******"});
+    }
+
+    public static int solution(String[] user_id, String[] banned_id) {
         boolean visited[] = new boolean[user_id.length]; // 방문체크
         ban_user_idx = new HashSet<String>(); // 중복제거
         dfs(user_id, banned_id, 0, visited); // 경우의 수 찾기
         int answer = ban_user_idx.size();
+        System.out.println(answer);
         return answer;
     }
 
-    private void dfs(String[] user_id, String[] banned_id, int ban_idx, boolean[] visited) {
-        if(ban_idx == banned_id.length) { // ban된 id 모두 선택
+    private static void dfs(String[] user_id, String[] banned_id, int ban_idx, boolean[] visited) {
+        if (ban_idx == banned_id.length) { // ban된 id 모두 선택
             StringBuilder user_idxs = new StringBuilder();
-            for(int i = 0; i < user_id.length; i++) {
-                if(visited[i]) {
+            for (int i = 0; i < user_id.length; i++) {
+                if (visited[i]) {
                     user_idxs.append(i); // 모든 인덱스를 이어붙이자
                 }
             }
             ban_user_idx.add(user_idxs.toString()); // 중복된 값 없는 모든 인덱스
-            return ;
+            return;
         }
-        for(int i = 0; i < user_id.length; i++) {
-            if(visited[i]) continue;
+        for (int i = 0; i < user_id.length; i++) {
+            if (visited[i])
+                continue;
             boolean flag = false;
             // 유저와 밴 된 유저의 길이가 같은 경우만 체크
-            if(user_id[i].length() == banned_id[ban_idx].length()) {
-                for(int s = 0; s < user_id[i].length(); s++) {
-                    if(banned_id[ban_idx].charAt(s) == '*') continue;
+            if (user_id[i].length() == banned_id[ban_idx].length()) {
+                for (int s = 0; s < user_id[i].length(); s++) {
+                    if (banned_id[ban_idx].charAt(s) == '*')
+                        continue;
                     // 하나라도 다른 글자가 포함되어 있다면 멈추고 다음 유저로
-                    if(user_id[i].charAt(s) != banned_id[ban_idx].charAt(s)) {
+                    if (user_id[i].charAt(s) != banned_id[ban_idx].charAt(s)) {
                         flag = true;
                         break;
                     }
                 }
-                if(!flag) { // 모든 글자가 맞았다
+                if (!flag) { // 모든 글자가 맞았다
                     visited[i] = true;
-                    dfs(user_id, banned_id, ban_idx+1, visited); // 다음 탐색
+                    dfs(user_id, banned_id, ban_idx + 1, visited); // 다음 탐색
                     visited[i] = false;
                 }
             }
