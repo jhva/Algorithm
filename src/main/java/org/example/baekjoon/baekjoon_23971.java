aeed2b0407083721b9e6ab59696d998f7a08f2a8
jhva <junghun8158@naver.com>
2023-06-26 16:52:32 +0900

    baekjoon_23971

--- src/main/java/org/example/baekjoon/baekjoon_23971.java
+++ src/main/java/org/example/baekjoon/baekjoon_23971.java
@@ -21,14 +21,12 @@ public class baekjoon_23971 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        W = Integer.parseInt(st.nextToken());
         H = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        int height = (H - 1) / (N + 1) + 1;
-        int width = (W - 1) / (M + 1) + 1;
-        answer = height * width;
+
+        answer = ((H - 1) / (N + 1) + 1) * ((W - 1) / (M + 1) + 1);
         System.out.println(answer);
 
     }
