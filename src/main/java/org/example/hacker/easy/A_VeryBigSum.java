d422ef2946ff49cf3cd4fa21234d4b4ce73b7024
jhva <junghun8158@naver.com>
2023-08-25 11:09:10 +0900

    :sparkles: TIL: Mini_max Sum

--- src/main/java/org/example/hacker/easy/A_VeryBigSum.java
+++ src/main/java/org/example/hacker/easy/A_VeryBigSum.java
@@ -1,19 +1,20 @@
-import java.util.*;
+package org.example.hacker.easy;
 
-public class A_VeryBigSum{
+import java.util.List;
 
-    
+public class A_VeryBigSum {
+
     public static void main(String[] args) {
-            solution(List.of(1000000001 ,1000000002 ,1000000003,1000000004,,1000000005));
+        solution(List.of(1000000001, 1000000002, 1000000003, 1000000004, 1000000005));
     }
 
-    public static int solution(List<Integer> ar){
+    public static long solution(List<Integer> ar) {
         long ans = 0;
-    
-        for(int i=0; i<ar.size(); i++){
+
+        for (int i = 0; i < ar.size(); i++) {
             ans += ar.get(i);
         }
-        
+
         return ans;
     }
 }
--- src/main/java/org/example/hacker/easy/CompareTheTriplets.java
+++ src/main/java/org/example/hacker/easy/CompareTheTriplets.java
@@ -1,10 +1,9 @@
-import java.util.*;
+import java.util.List;
 
-public class CompareTheTriplets{
+public class CompareTheTriplets {
 
-    
     public static void main(String[] args) {
-        solution(List.of(1,2,3); //example inputs 
+        solution(List.of(1, 2, 3)); //example inputs
 
         //original inputs 
         // https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
@@ -12,13 +11,12 @@ public class CompareTheTriplets{
         //3 6 10
     }
 
-    public static int solution(List<Integer>ar){
-        int ans= 0; 
-        for(int i=0; i<ar.size(); i++){
-            ans+=ar.get(i);
+    public static int solution(List<Integer> ar) {
+        int ans = 0;
+        for (int i = 0; i < ar.size(); i++) {
+            ans += ar.get(i);
         }
-        
-        
+
         return ans;
     }
 }
